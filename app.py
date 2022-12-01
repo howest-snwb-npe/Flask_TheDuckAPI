a598c6e8dfe157aa94896a769f876a37fda33889
koen-db <koen.de.bruyne@howest.be>
2022-12-01 14:45:02 +0100

    Add status-code images

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import base64
 
 app = Flask(__name__)
 
@@ -12,6 +13,12 @@ def random():
     r = requests.get("https://random-d.uk/api/v2/random")
     photo_url = r.json()["url"]
     return render_template("random.html", photo_url=photo_url)
+
+@app.route("/status-code",methods=["POST"])
+def status_code():
+    status_code = request.form.get("status-code")
+    photo_url = f"https://random-d.uk/api/v2/http/{status_code}"
+    return render_template("status-code.html", photo_url=photo_url)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
