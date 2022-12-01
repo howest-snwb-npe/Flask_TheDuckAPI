36ee046b13dc0c2e230a9f31e7a99aeacb5d9033
koen-db <koen.de.bruyne@howest.be>
2022-12-01 13:39:24 +0100

    Add a second page and a form

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/")
 def root():
     return render_template("index.html")
+
+@app.route("/random")
+def random():
+    return render_template("random.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
